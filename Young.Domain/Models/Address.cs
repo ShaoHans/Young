23e433004b06f65d87217bc7568fba754b5f2c91
shz <shz@qq.com>
2018-10-19 18:14:19 +0800

    update

--- Young.Domain/Models/Address.cs
+++ Young.Domain/Models/Address.cs
@@ -6,11 +6,11 @@ namespace Young.Domain.Models
 {
     public class Address
     {
-        public int ProvinceId { get; set; }
+        public int ProvinceCode { get; set; }
 
         public string ProvinceName { get; set; }
 
-        public int CityId { get; set; }
+        public int CityCode { get; set; }
 
         public string CityName { get; set; }
     }
--- Young.Infrastructure/EntityConfigurations/ReaderConfiguration.cs
+++ Young.Infrastructure/EntityConfigurations/ReaderConfiguration.cs
@@ -13,12 +13,17 @@ namespace Young.Infrastructure.EntityConfigurations
         {
             builder.ToTable("Reader").HasKey(r => r.Id);
             builder.Property(b => b.Id).HasColumnName("ReaderId").ValueGeneratedOnAdd();
-            /* 迁移的时候有bug
-            builder.OwnsOne(b => b.Local).Property(l => l.ProvinceName).HasColumnType("varchar(30)");
-            builder.OwnsOne(b => b.Local).Property(l => l.CityName).HasColumnType("varchar(30)");
-            builder.OwnsOne(b => b.Nation).Property(l => l.ProvinceName).HasColumnType("varchar(30)");
-            builder.OwnsOne(b => b.Nation).Property(l => l.CityName).HasColumnType("varchar(30)");
-            */
+            /* 迁移的时候有bug*/
+            //builder.OwnsOne(b => b.Local).Property(l => l.ProvinceName).HasColumnName("LocalProvinceName").HasColumnType("varchar(30)");
+            //builder.OwnsOne(b => b.Local).Property(l => l.CityName).HasColumnName("LocalCityName").HasColumnType("varchar(30)");
+            //builder.OwnsOne(b => b.Nation).Property(l => l.ProvinceName).HasColumnName("NationProvinceName").HasColumnType("varchar(30)");
+            //builder.OwnsOne(b => b.Nation).Property(l => l.CityName).HasColumnName("NationCityName").HasColumnType("varchar(30)");
+
+            //builder.OwnsOne(b => b.Local);
+            builder.OwnsOne(b => b.Nation, nation =>
+            {
+                nation.Ignore(n => n.ProvinceCode);
+            });
         }
     }
 }
